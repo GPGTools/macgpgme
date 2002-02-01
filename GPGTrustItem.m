573b97682f409e2c7f5a184db5c5167056a06b33
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2002-02-01 17:23:01 +0000

    Updated documentation; added some assertions
    git-svn-id: http://macgpg.svn.sourceforge.net/svnroot/macgpg/GPGME/trunk@422 c19e94ca-3d1d-0410-add0-bf5d5e3bda8e

--- GPGTrustItem.m
+++ GPGTrustItem.m
@@ -5,7 +5,7 @@
 //  Created by davelopper@users.sourceforge.net on Tue Aug 14 2001.
 //
 //
-//  Copyright (C) 2001 Mac GPG Project.
+//  Copyright (C) 2001-2002 Mac GPG Project.
 //  
 //  This code is free software; you can redistribute it and/or modify it under
 //  the terms of the GNU General Public License as published by the Free
@@ -33,10 +33,10 @@
 
 @implementation GPGTrustItem
 /*"
- * You should never need to instantiate objects of that class. GPGContext does
- * it for you.
+ * #GPGTrustItem instances are returned by #{-[GPGContext trustListEnumeratorForSearchPattern:maximumLevel:]};
+ * you should never need to instantiate objects of that class.
  *
- * NOTE: the underlying implementation is not yet well defined, dixit Werner Koch.
+ * #CAUTION: the trust items interface is experimental.
 "*/
 
 - (void) dealloc
@@ -49,10 +49,14 @@
 }
 
 - (NSString *) keyID
+/*"
+ * Returns the %{key ID} of the %key refered by the trust item.
+"*/
 {
     const char	*aCString = gpgme_trust_item_get_string_attr(_trustItem, GPGME_ATTR_KEYID, NULL, 0);
     NSString	*aString = nil;
 
+    NSAssert(aCString != NULL, @"### Invalid key.");
     if(aCString != NULL)
         aString = [NSString stringWithUTF8String:aCString];
 
@@ -79,9 +83,13 @@
 }
 
 - (GPGValidity) ownerTrust
+/*"
+ * #CAUTION: not yet working.
+"*/
 {
     const char	*aCString = gpgme_trust_item_get_string_attr(_trustItem, GPGME_ATTR_OTRUST, NULL, 0);
 
+    NSAssert(aCString != NULL, @"### Invalid key.");
     if(aCString != NULL){
         NSAssert1(strlen(aCString) == 1, @"### We cannot decode this ownerTrust value: %s", aCString);
 
@@ -92,10 +100,14 @@
 }
 
 - (NSString *) userID
+/*"
+ * Returns the %{user ID} associated with the trust item.
+"*/
 {
     const char	*aCString = gpgme_trust_item_get_string_attr(_trustItem, GPGME_ATTR_USERID, NULL, 0);
     NSString	*aString = nil;
 
+    NSAssert(aCString != NULL, @"### Invalid key.");
     if(aCString != NULL)
         aString = [NSString stringWithUTF8String:aCString];
 
@@ -104,13 +116,14 @@
 
 - (GPGValidity) validity
 /*"
- * Returns the computed validity.
+ * Returns the computed validity associated with the trust item.
 "*/
 {
     const char	*aCString = gpgme_trust_item_get_string_attr(_trustItem, GPGME_ATTR_VALIDITY, NULL, 0);
 
+    NSAssert(aCString != NULL, @"### Invalid key.");
     if(aCString != NULL){
-        NSAssert1(strlen(aCString) == 1, @"We cannot decode this validity value: %s", aCString);
+        NSAssert1(strlen(aCString) == 1, @"### We cannot decode this validity value: %s", aCString);
 
         return [self validityFromCharacter:aCString[0]];
     }
@@ -119,14 +132,37 @@
 }
 
 - (int) level
+/*"
+ * Returns the trust level of the trust item.
+"*/
 {
-    return gpgme_trust_item_get_int_attr(_trustItem, GPGME_ATTR_LEVEL, NULL, 0);
+    int	level = gpgme_trust_item_get_int_attr(_trustItem, GPGME_ATTR_LEVEL, NULL, 0);
+    
+    // It seems that level=0 is a valid value (ultimately trusted?)
+//    NSAssert(level != 0, @"### Invalid key.");
+    
+    return level;
 }
 
 - (int) type
+/*"
+ * Returns the type of the trust item.
+ *
+ * #CAUTION: not yet working.
+"*/
 {
     // What is the <type> attribute??? Asked to Werner: "do not use GpgmeTrustItem yet"
-    return gpgme_trust_item_get_int_attr(_trustItem, GPGME_ATTR_TYPE, NULL, 0);
+    int	type = gpgme_trust_item_get_int_attr(_trustItem, GPGME_ATTR_TYPE, NULL, 0);
+    
+    NSAssert(type != 0, @"### Invalid key.");
+    
+    return type;
 }
 
+#warning TODO
+/*
+    We could also implement the following calls:
+    - (GPGKey *) key
+    (we need to create a local context to get the named key; key should be cached)
+*/
 @end
