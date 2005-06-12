aa2ba3075212a6b3c76926399287b460b84972ad
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2005-06-12 19:56:06 +0000

    Renamed GPGME -> MacGPGME; refactoring + adapted to Xcode 2.1 (should work with Xcode 2.0, though untested)
    git-svn-id: http://macgpg.svn.sourceforge.net/svnroot/macgpg/GPGME/trunk@1677 c19e94ca-3d1d-0410-add0-bf5d5e3bda8e

--- frameworkEmbedding.sh
+++ frameworkEmbedding.sh
@@ -6,10 +6,12 @@
 # rewrite install_name in the framework
 # note we do not bother to change the debug or profile variants since those are never directly linked against at static link time.
 
-GPGME_FRAMEWORK_VERSION=1.0.2
+FRAMEWORK_NAME=MacGPGME
+FRAMEWORK_VERSION=1.0.2
 
-chmod u+w "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}/Contents/Frameworks/GPGME.framework/Versions/${GPGME_FRAMEWORK_VERSION}/GPGME"
-install_name_tool -id "@executable_path/../Frameworks/GPGME.framework/Versions/${GPGME_FRAMEWORK_VERSION}/GPGME" "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}/Contents/Frameworks/GPGME.framework/Versions/${GPGME_FRAMEWORK_VERSION}/GPGME"
+cd "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}/Contents/Frameworks/${FRAMEWORK_NAME}.framework/Versions/${FRAMEWORK_VERSION}/"
+chmod u+w "${FRAMEWORK_NAME}"
+install_name_tool -id "@executable_path/../Frameworks/${FRAMEWORK_NAME}.framework/Versions/${FRAMEWORK_VERSION}/${FRAMEWORK_NAME}" "${FRAMEWORK_NAME}"
 
 # rewrite install_name in the app
-install_name_tool -change "/Library/Frameworks/GPGME.framework/Versions/${GPGME_FRAMEWORK_VERSION}/GPGME" "@executable_path/../Frameworks/GPGME.framework/Versions/${GPGME_FRAMEWORK_VERSION}/GPGME" "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}/Contents/MacOS/${PRODUCT_NAME}"
+install_name_tool -change "/Library/Frameworks/${FRAMEWORK_NAME}.framework/Versions/${FRAMEWORK_VERSION}/${FRAMEWORK_NAME}" "@executable_path/../Frameworks/${FRAMEWORK_NAME}.framework/Versions/${FRAMEWORK_VERSION}/${FRAMEWORK_NAME}" "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}/Contents/MacOS/${PRODUCT_NAME}"
