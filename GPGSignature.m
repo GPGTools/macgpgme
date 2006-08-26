ddae63e4f861344f8bacbcbc55085e8dee4394d3
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2006-08-26 18:01:18 +0000

    Now uses HeaderDoc for documentation generation Added GPGPKATrust enum, -pkaTrust, -pkaAddress
    git-svn-id: http://macgpg.svn.sourceforge.net/svnroot/macgpg/GPGME/trunk@1805 c19e94ca-3d1d-0410-add0-bf5d5e3bda8e

--- GPGSignature.m
+++ GPGSignature.m
@@ -5,7 +5,7 @@
 //  Created by davelopper at users.sourceforge.net on Sun Jul 14 2002.
 //
 //
-//  Copyright (C) 2001-2005 Mac GPG Project.
+//  Copyright (C) 2001-2006 Mac GPG Project.
 //  
 //  This code is free software; you can redistribute it and/or modify it under
 //  the terms of the GNU Lesser General Public License as published by the Free
@@ -34,13 +34,6 @@
 
 
 @implementation GPGSignature
-/*"
- * #GPGSignature instances are returned by #{-[GPGContext signatures]}; you
- * should never need to instantiate yourself objects of that class. Signatures
- * are also returned after a signing operation, but in this case, currently,
- * not all attributes have significant values: you can count only on
- * -algorithm, -hashAlgorithm, -signatureClass, -creationDate and -fingerprint
-"*/
 
 + (BOOL) accessInstanceVariablesDirectly
 {
@@ -55,43 +48,23 @@
     [_notations release];
     [_policyURLs release];
     [_signatureNotations release];
+    [_pkaAddress release];
 
     [super dealloc];
 }
 
 - (id) copyWithZone:(NSZone *)zone
-/*"
- * Returns the same instance, retained. #GPGSignature instances are
- * (currently) immutable.
- *
- * #WARNING: zone is not taken in account.
-"*/
 {
     // Implementation is useful to allow use of GPGSignature instances as keys in NSMutableDictionary instances.
     return [self retain];
 }
 
 - (NSString *) fingerprint
-/*"
- * Returns signer's key %fingerprint (or keyID!).
-"*/
 {
     return _fingerprint;
 }
 
 - (NSString *) formattedFingerprint
-/*"
- * If -fingerprint returns a fingerprint, returns %{fingerprint} in hex digit
- * form, formatted like this:
- *
- * XXXX XXXX XXXX XXXX XXXX  XXXX XXXX XXXX XXXX XXXX
- *
- * or
- *
- * XX XX XX XX XX XX XX XX  XX XX XX XX XX XX XX XX
- *
- * If -fingerprint returns a keyID, return keyID prefixed by 0x.
-"*/
 {
     NSString	*aString = [self fingerprint];
 
@@ -104,160 +77,83 @@
 }
 
 - (GPGError) validityError
-/*"
- * If a signature is not valid, this provides a reason why. Not used for new
- * signatures.
-"*/
 {
     return _validityError;
 }
 
 - (BOOL) wrongKeyUsage
-/*"
- * Returns YES if the key was not used according to its policy. Not used for
- * new signatures.
-"*/
 {
     return _wrongKeyUsage;
 }
 
 - (NSCalendarDate *) creationDate
-/*"
- * Returns %signature creation date. Returns nil when not available or
- * invalid.
-"*/
 {
     return _creationDate;
 }
 
 - (NSCalendarDate *) expirationDate
-/*"
- * Returns %signature expiration date. Returns nil if signature does not
- * expire. Not used for new signatures.
-"*/
 {
     return _expirationDate;
 }
 
 - (GPGValidity) validity
-/*"
- * Returns %signature's validity. Not used for new signatures.
- *
- * Note that a signature's validity is never #GPGValidityUltimate, because
- * #GPGValidityUltimate is reserved for key certification, not for signatures.
-"*/
 {
     return _validity;
 }
 
 - (NSString *) validityDescription
-/*"
- * Returns %signature's validity in localized human readable form. Not used
- * for new signatures.
-"*/
 {
     return GPGValidityDescription([self validity]);
 }
 
 - (GPGError) status
-/*"
- * Returns %signature status. In particular, the following status codes are of
- * interest:
- * _{GPGErrorNoError             This status indicates that the signature is
- *                               valid. For the combined result this status
- *                               means that all signatures are valid.}
- * _{GPGErrorSignatureExpired    This status indicates that the signature is
- *                               valid but expired. For the combined result this
- *                               status means that all signatures are valid and
- *                               expired.}
- * _{GPGErrorKeyExpired          This status indicates that the signature is
- *                               valid but the key used to verify the signature
- *                               has expired. For the combined result this
- *                               status means that all signatures are valid and
- *                               all keys are expired.}
- * _{GPGErrorCertificateRevoked  This status indicates that the signature is
- *                               valid but the key used  to verify the signature
- *                               has been revoked. For the combined result this
- *                               status means that all signatures are valid and
- *                               all keys are  revoked.}
- * _{GPGErrorBadSignature        This status indicates that the signature is
- *                               invalid. For the combined result this status
- *                               means that all signatures are invalid.}
- * _{GPGErrorNoPublicKey         This status indicates that the signature could
- *                               not be verified due to a missing key. For the
- *                               combined result this status means that all
- *                               signatures could not be checked due to missing
- *                               keys.}
- * _{GPGErrorGeneralError        This status indicates that there was some other
- *                               error which prevented the signature
- *                               verification.}
- * Not used for new signatures.
-"*/
 {
     return _status;
 }
 
 - (GPGSignatureSummaryMask) summary
-/*"
- * Returns a mask giving a summary of the signature status. Not used for new
- * signatures.
-"*/
 {
     return _summary;
 }
 
 - (GPGPublicKeyAlgorithm) algorithm
-/*"
- * Returns the public key algorithm used to create the signature.
-"*/
 {
     return _algorithm;
 }
 
 - (NSString *) algorithmDescription
-/*"
- * Returns the localized description of the public key algorithm used to create
- * the signature.
-"*/
 {
     return GPGLocalizedPublicKeyAlgorithmDescription([self algorithm]);
 }
 
 - (GPGHashAlgorithm) hashAlgorithm
-/*"
- * Returns the hash algorithm used for the signature.
-"*/
 {
     return _hashAlgorithm;
 }
 
 - (NSString *) hashAlgorithmDescription
-/*"
- * Returns the localized description of the hash algorithm used for the
- * signature.
-"*/
 {
     return GPGLocalizedHashAlgorithmDescription([self hashAlgorithm]);
 }
 
 - (unsigned int) signatureClass
-/*"
- * Returns the signature class of a %{key signature} or a new signature. The
- * meaning is specific to the crypto engine.
- *
- * This attribute is not (yet?) available for signatures returned after a
- * verification operation.
-"*/
 {
     return _signatureClass;
 }
 
 - (NSArray *) signatureNotations
-/*"
- * Returns all signature notations (notation data and policy URLs).   
-"*/
 {
     return _signatureNotations;
+}
+
+- (GPGPKATrust) pkaTrust
+{
+    return _pkaTrust;
+}
+
+- (NSString *) pkaAddress
+{
+    return _pkaAddress;
 }
 
 @end
@@ -266,25 +162,11 @@
 @implementation GPGSignature(GPGSignatureDeprecated)
 
 - (NSDictionary *) notations
-/*"
- * Returns a dictionary of %{notation data} key-value pairs. A notation is a
- * key/value pair that is added to the content, it can be anything. Value
- * is returned as an NSString instance. Not used for new signatures.
- *
- * #DEPRECATED: use -signatureNotations instead.
-"*/
 {
     return _notations;
 }
 
 - (NSArray *) policyURLs
-/*"
- * Returns an array of %{policy URLs} as NSString instances. A policy URL is
- * an URL to a document that documents the persons policy in signing other
- * peoples keys. Not used for new signatures.
- *
- * #DEPRECATED: use -signatureNotations instead.
-"*/
 {
     return _policyURLs;
 }
@@ -343,6 +225,8 @@
         _algorithm = signature->pubkey_algo;
         _hashAlgorithm = signature->hash_algo;
         _signatureClass = 0; // Unsignificant value
+        _pkaTrust = signature->pka_trust;
+        _pkaAddress = [GPGStringFromChars(signature->pka_address) retain];
     }
 
     return self;
