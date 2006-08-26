09f04cd20233bce7883c8810f58d1ad027cb5b41
davelopper <davelopper@c19e94ca-3d1d-0410-add0-bf5d5e3bda8e>
2006-08-26 17:26:13 +0000

    Now uses HeaderDoc for documentation generation Changed -debugDescription
    git-svn-id: http://macgpg.svn.sourceforge.net/svnroot/macgpg/GPGME/trunk@1799 c19e94ca-3d1d-0410-add0-bf5d5e3bda8e

--- GPGEngine.h
+++ GPGEngine.h
@@ -5,7 +5,7 @@
 //  Created by davelopper at users.sourceforge.net on Tue Aug 14 2001.
 //
 //
-//  Copyright (C) 2001-2005 Mac GPG Project.
+//  Copyright (C) 2001-2006 Mac GPG Project.
 //  
 //  This code is free software; you can redistribute it and/or modify it under
 //  the terms of the GNU Lesser General Public License as published by the Free
@@ -44,42 +44,184 @@ extern "C" {
 @class GPGContext;
 
 
-/*"
- * The #GPGProtocol type specifies the set of possible protocol values that
- * are supported by MacGPGME. The following protocols are supported:
- * _{GPGOpenPGPProtocol  Default protocol. OpenPGP is implemented by GnuPG,
- *                       the GNU Privacy Guard. This is the first protocol
- *                       that was supported by MacGPGME.}
- * _{GPGCMSProtocol      CMS (Cryptographic Message Syntax) is implemented by
- *                       GpgSM, the S/MIME implementation for GnuPG.
- *                       #CAUTION: currently unsupported on MacOS X.}
-"*/
+/*!
+ *  @typedef    GPGProtocol
+ *  @abstract   Specifies the set of possible protocol values that are
+ *              supported by MacGPGME.
+ *  @constant   GPGOpenPGPProtocol Default protocol. OpenPGP is implemented by
+ *                                 GnuPG, the GNU Privacy Guard. This is the
+ *                                 first protocol that was supported by
+ *                                 MacGPGME.
+ *  @constant   GPGCMSProtocol     CMS (Cryptographic Message Syntax) is 
+ *                                 implemented by GpgSM, the S/MIME
+ *                                 implementation for GnuPG.
+ *                                 <strong>WARNING:</strong> currently 
+ *                                 unsupported on MacOS X.
+ */
 typedef enum {
     GPGOpenPGPProtocol = 0,
     GPGCMSProtocol     = 1
 } GPGProtocol;
 
 
-@interface GPGEngine : GPGObject /*"NSObject"*/
+/*!
+ *  @class      GPGEngine
+ *  @abstract   Represents the back-end engine providing cryptographic 
+ *              functionalities.
+ *  @discussion MacGPGME supports several cryptographic protocols, however, it 
+ *              does not implement them. Rather it uses back-ends (also called 
+ *              engines) which implement the protocol. MacGPGME uses 
+ *              inter-process communication to pass data back and forth between
+ *              the application and the back-end, but the details of the
+ *              communication protocol and invocation of the back-end is 
+ *              completely hidden by the interface. All complexity is handled by
+ *              MacGPGME. Where an exchange of information between the
+ *              application and the back-end is necessary, MacGPGME provides the
+ *              necessary callback method hooks and further interfaces.
+ *
+ *              You can modify some parameters of the engines, like their
+ *              executable path and their home directory. This can be done
+ *              either on the default engines, or the engines proper to a
+ *              <code>@link //macgpg/occ/cl/GPGContext GPGContext@/link</code>
+ *              (see GPGContext's <code>@link //macgpg/occ/instm/GPGContext/engine engine@/link</code>).
+ */
+@interface GPGEngine : GPGObject
 {
     GPGContext  *_context; // Not retained
 }
 
+/*!
+ *  @method     checkVersionForProtocol:
+ *  @abstract   Checks that the engine implementing the protocol <i>protocol</i> 
+ *              is installed in the expected path and meets the version
+ *              requirement of MacGPGME.
+ *  @discussion This method returns <code>@link //macgpg/c/econst/GPGErrorNoError GPGErrorNoError@/link</code>
+ *              if the engine is available and an error with code
+ *              <code>@link //macgpg/c/econst/GPGErrorInvalidEngine GPGErrorInvalidEngine@/link</code>
+ *              if it is not.
+ *  @param      protocol The engine protocol
+ */
 + (GPGError) checkVersionForProtocol:(GPGProtocol)protocol;
+
+/*!
+ *  @method     checkFrameworkVersion:
+ *  @abstract   Checks that the version of the framework is at minimum the 
+ *              requested one and returns the version string.
+ *  @discussion Returns nil if the condition is not met or 
+ *              <i>requiredVersion</i> is not a valid version number. If
+ *              <i>requiredVersion</i> is nil, no check is done and the version
+ *              string is simply returned.
+ *
+ *              Note that this check is automatically performed before any 
+ *              MacGPGME object/function is used; it is called from 
+ *              <code>@link //macgpg/occ/clm/GPGObject/initialize initialize@/link</code>
+ *              (GPGObject).
+ *  @param      requiredVersion Version number or nil
+ */
 + (NSString *) checkFrameworkVersion:(NSString *)requiredVersion;
+
+/*!
+ *  @method     defaultHomeDirectoryForProtocol:
+ *  @abstract   Returns the default home directory (constant) for 
+ *              <i>protocol</i>.
+ *  @discussion <strong>WARNING:</strong> currently implemented only for
+ *              OpenPGP.
+ *  @param      protocol The engine protocol
+ */
 + (NSString *) defaultHomeDirectoryForProtocol:(GPGProtocol)protocol;
 
+/*!
+ *  @method     availableEngines
+ *  @abstract   Returns an array of GPGEngine objects.
+ *  @discussion Each engine in the array describes one configured back-end.
+ */
 + (NSArray *) availableEngines;
+
+/*!
+ *  @method     engineForProtocol:
+ *  @abstract   Returns the engine for the given protocol.
+ *  @discussion Convenience method.
+ *  @param      protocol The engine protocol
+ */
 + (GPGEngine *) engineForProtocol:(GPGProtocol)protocol;
 
+/*!
+ *  @method     engineProtocol
+ *  @abstract   Returns the protocol for which the crypto engine is used.
+ *  @discussion You can convert this to a string with 
+ *              <code>@link //macgpg/c/func/GPGProtocolDescription GPGProtocolDescription@/link</code>
+ *              or <code>@link //macgpg/c/func/GPGLocalizedProtocolDescription GPGLocalizedProtocolDescription@/link</code> for printing.
+ */
 - (GPGProtocol) engineProtocol;
+
+/*!
+ *  @method     version
+ *  @abstract   Returns the crypto engine version.
+ *  @discussion This is a string containing the version number of the crypto 
+ *              engine. It might be nil if the version number can not be
+ *              determined, for example because the executable doesn't exist or
+ *              is invalid.
+ */
 - (NSString *) version;
+
+/*!
+ *  @method     requestedVersion
+ *  @abstract   Returns the minimum required version of the crypto engine.
+ *  @discussion Returns a string containing the minimum required version number
+ *              of the crypto engine for MacGPGME to work correctly. This is the
+ *              version number that <code>@link checkVersionForProtocol: checkVersionForProtocol:@/link</code>
+ *              verifies against. Currently, it is never nil, but using nil is
+ *              reserved for future use, so always check before you use it.
+ */
 - (NSString *) requestedVersion;
 
+/*!
+ *  @method     executablePath
+ *  @abstract   Returns a string holding the path to the executable of the 
+ *              crypto engine.
+ *  @discussion Currently, never returns nil, but using nil is reserved for
+ *              future use, so always check before you use it.
+ *
+ *              At startup, for OpenPGP, the default path is set according to 
+ *              user defaults, when available. MacGPGME reads user defaults 
+ *              domain <code>@link //macgpg/c/data/GPGUserDefaultsSuiteName GPGUserDefaultsSuiteName@/link</code>,
+ *              and default executable path is extracted from key
+ *              <code>@link //macgpg/c/data/GPGOpenPGPExecutablePathKey GPGOpenPGPExecutablePathKey@/link</code>. When not available in defaults,
+ *              value is <code>/usr/local/bin/gpg</code>.
+ */
 - (NSString *) executablePath;
+
+/*!
+ *  @method     setExecutablePath:
+ *  @abstract   Sets the path to the executable of the crypto engine.
+ *  @discussion Currently may never be nil.
+ *  @exception  <code>@link //macgpg/c/data/GPGException GPGException@/link</code>
+ *              (<code>@link //macgpg/c/econst/GPGErrorInvalidEngine GPGErrorInvalidEngine@/link</code>)
+ *              when no engine is found at <i>executablePath</i>.
+ *  @param      executablePath The path to the engine executable.
+ */
 - (void) setExecutablePath:(NSString *)executablePath;
 
+/*!
+ *  @method     homeDirectory
+ *  @abstract   Returns the directory name of the crypto engine's configuration 
+ *              directory.
+ *  @discussion If it is nil, then the default directory is used; for the 
+ *              OpenPGP engine, it is <code>$HOME/.gnupg</code>, or 
+ *              <code>$GNUPGHOME</code> if environment variable is set.
+ */
 - (NSString *) homeDirectory;
+
+/*!
+ *  @method     setHomeDirectory:
+ *  @abstract   Sets the directory name of the crypto engine's configuration 
+ *              directory.
+ *  @discussion Sets the directory name of the crypto engine's configuration
+ *              directory. If it is nil, then the default directory is used; for 
+ *              the OpenPGP engine, it is <code>$HOME/.gnupg</code>, or
+ *              <code>$GNUPGHOME</code> if environment variable is set.
+ *  @param      homeDirectory Path to engine home directory.
+ */
 - (void) setHomeDirectory:(NSString *)homeDirectory;
 
 @end
--- GPGEngine.m
+++ GPGEngine.m
@@ -5,7 +5,7 @@
 //  Created by davelopper at users.sourceforge.net on Tue Aug 14 2001.
 //
 //
-//  Copyright (C) 2001-2005 Mac GPG Project.
+//  Copyright (C) 2001-2006 Mac GPG Project.
 //  
 //  This code is free software; you can redistribute it and/or modify it under
 //  the terms of the GNU Lesser General Public License as published by the Free
@@ -39,21 +39,6 @@
 
 
 @implementation GPGEngine
-/*"
- * MacGPGME supports several cryptographic protocols, however, it does not
- * implement them. Rather it uses backends (also called engines) which
- * implement the protocol. MacGPGME uses inter-process communication to pass
- * data back and forth between the application and the backend, but the details
- * of the communication protocol and invocation of the backend is completely
- * hidden by the interface. All complexity is handled by MacGPGME. Where an
- * exchange of information between the application and the backend is
- * necessary, MacGPGME provides the necessary callback method hooks and further
- * interfaces.
- *
- * You can modify some parameters of the engines, like their executable path and
- * their home directory. This can be done either on the default engines, or the
- * engines proper to a GPGContext (see -[GPGContext engine]).
-"*/
 
 + (BOOL) needsPointerUniquing
 {
@@ -61,31 +46,11 @@
 }
 
 + (GPGError) checkVersionForProtocol:(GPGProtocol)protocol
-/*"
- * Checks that the engine implementing the protocol protocol is installed
- * in the expected path and meets the version requirement of MacGPGME.
- *
- * This method returns #GPGErrorNoError if the engine
- * is available and an error whose code is #GPGErrorInvalidEngine if it is not.
- *
- * #WARNING This method does not work correctly in 1.1 when you change the
- * default executable path!
-"*/
 {
-#warning This method does not work correctly in 1.1 when you change the default executable path!
     return gpgme_engine_check_version(protocol);
 }
 
 + (NSString *) checkFrameworkVersion:(NSString *)requiredVersion
-/*"
- * Checks that the version of the framework is at minimum the requested one
- * and returns the version string; returns nil if the condition is not met or
- * requiredVersion is not a valid version number. If requiredVersion is nil,
- * no check is done and the version string is simply returned.
- *
- * Note that this check is automatically performed before any MacGPGME 
- * object/function is used; it is called from #{+[GPGObject initialize]}.
-"*/
 {
     const char	*aCString;
 
@@ -95,10 +60,6 @@
 }
 
 + (NSArray *) availableEngines
-/*"
- * Returns an array of GPGEngine instances. Each instance describes one
- * configured backend.
-"*/
 {
     gpgme_engine_info_t	anEngine = NULL;
     gpgme_error_t		anError = gpgme_get_engine_info(&anEngine); // The memory for the info structures is allocated the first time this function is invoked, and must not be freed by the caller.
@@ -110,9 +71,6 @@
 }
 
 + (GPGEngine *) engineForProtocol:(GPGProtocol)protocol
-/*"
- * Convenience method. Returns the engine for the given protocol.
-"*/
 {
     NSEnumerator    *engineEnum = [[self availableEngines] objectEnumerator];
     GPGEngine       *anEngine;
@@ -125,11 +83,6 @@
 }
 
 + (NSString *) defaultHomeDirectoryForProtocol:(GPGProtocol)protocol
-/*"
- * Returns the default home directory (constant) for protocol.
- *
- * #WARNING: currently implemented only for OpenPGP.
-"*/
 {
     static NSMutableDictionary *defaultHomeDirectoryPerProtocol = nil;
     NSNumber                    *aProtocol = [NSNumber numberWithInt:protocol];
@@ -153,11 +106,6 @@
 }
 
 - (GPGProtocol) engineProtocol
-/*"
- * Returns the protocol for which the crypto engine is used. You can convert
- * this to a string with #{GPGProtocolDescription()} or
- * #{GPGLocalizedProtocolDescription()} for printing
-"*/
 {
     NSAssert(_context != INVALID_CONTEXT, @"### GPGEngine instance was associated to a GPGContext that has been freed.");
 
@@ -165,16 +113,6 @@
 }
 
 - (NSString *) executablePath
-/*"
- * Returns a string holding the file name of the executable of the crypto
- * engine. Currently, it is never nil, but using nil is reserved for future
- * use, so always check before you use it.
- *
- * At startup, for OpenPGP, the default path is set according to user defaults,
- * when available. MacGPGME reads user defaults domain 
- * #GPGUserDefaultsSuiteName, and default executable path is extracted from key
- * #GPGOpenPGPExecutablePathKey.
-"*/
 {
     NSAssert(_context != INVALID_CONTEXT, @"### GPGEngine instance was associated to a GPGContext that has been freed.");
 
@@ -182,12 +120,6 @@
 }
 
 - (void) setExecutablePath:(NSString *)executablePath
-/*"
- * Sets the path to the executable of the crypto engine. Currently may never be
- * nil.
- *
- * Can raise a #GPGException when no engine is found at executablePath.
-"*/
 {
     NSParameterAssert(executablePath != nil);
     
@@ -217,11 +149,6 @@
 }
 
 - (NSString *) version
-/*"
- * This is a string containing the version number of the crypto engine. It
- * might be nil if the version number can not be determined, for example
- * because the executable doesn't exist or is invalid.
-"*/
 {
     const char	*aCString;
 
@@ -232,12 +159,6 @@
 }
 
 - (NSString *) requestedVersion
-/*"
- * Returns a string containing the minimum required version number of the
- * crypto engine for MacGPGME to work correctly. This is the version number that
- * #{+checkVersionForProtocol:} verifies against. Currently, it is never nil,
- * but using nil is reserved for future use, so always check before you use it.
-"*/
 {
     const char	*aCString;
 
@@ -248,11 +169,6 @@
 }
 
 - (NSString *) homeDirectory
-/*"
- * Returns the directory name of the crypto engine's configuration directory. If
- * it is nil, then the default directory is used, i.e. $HOME/.gnupg for the 
- * OpenPGP engine, or $GNUPGHOME environment variable if set.
-"*/
 {
     const char	*aCString;
     
@@ -263,11 +179,6 @@
 }
 
 - (void) setHomeDirectory:(NSString *)homeDirectory
-/*"
- * Sets the directory name of the crypto engine's configuration directory. If it
- * is nil, then the default directory is used, i.e. $HOME/.gnupg for the OpenPGP
- * engine, or $GNUPGHOME environment variable if set.
-"*/
 {
     NSString    *myHomeDirectory = [self homeDirectory];
     
@@ -297,7 +208,7 @@
     if(_context == INVALID_CONTEXT)
         return [NSString stringWithFormat:@"<%@ %p> [freed context]", NSStringFromClass([self class]), self];
     else
-        return [NSString stringWithFormat:@"<%@ %p> %@ (min. %@), %@ (%@), %@ - %@", NSStringFromClass([self class]), self, GPGProtocolDescription([self engineProtocol]), [self requestedVersion], [self executablePath], [self version], [self homeDirectory], (_context != nil ? _context : @"global")];
+        return [NSString stringWithFormat:@"<%@ %p> %@ (min. %@), %@ (%@), %@ - %@", NSStringFromClass([self class]), self, GPGProtocolDescription([self engineProtocol]), [self requestedVersion], [self executablePath], [self version], [self homeDirectory], (_context != nil ? [_context description] : @"global")];
 }
 
 @end
